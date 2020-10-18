d368a5efac5bf9eb4c7dcb78d4d73083baf4494c
Irena Jurasek <irenaskydive@gmail.com>
2020-10-18 18:58:03 +0200

    Redirecting via useNavigation

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import useFetch from './services/useFetch';
 import Spinner from "./Spinner";
+import NotFound from './NotFound';
 
 export default function Detail() {
     const { id } = useParams();
+    const navigate = useNavigate();
     const { data: product, loading, error } = useFetch(`products/${id}`);
 
     if (loading) return <Spinner />;
+    if (!product) return <NotFound />;
     if (error) throw error;
 
     return (
@@ -15,6 +18,9 @@ export default function Detail() {
             <h1>{product.name}</h1>
             <p>{product.description}</p>
             <p id="price">${product.price}</p>
+            <p>
+                <button className="btn btn-primary" onClick={() => navigate("/cart")}>Add to cart</button>
+            </p>
             <img src={`/images/${product.image}`} alt={product.category} />
         </div>
     );
