3bdb316890af0ed20e796e67d91f8938e0e918b7
Irena Jurasek <irenaskydive@gmail.com>
2020-10-18 18:52:53 +0200

    Fetching based on URL

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter} from 'react-router-dom';
 ReactDOM.render(
     <ErrorBoundary>
       <BrowserRouter>
-      <App /> 
+        <App /> 
       </BrowserRouter>
     </ErrorBoundary>, 
     document.getElementById("root"));
